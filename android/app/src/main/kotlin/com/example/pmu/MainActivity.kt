4c706bf4352f11002f1a5b06f3bddde1948e2a72
qkrlnt <Alexander@DESKTOP-N4NPCJU>
2025-09-08 15:49:18 +0400

    init commit

---
+++ android/app/src/main/kotlin/com/example/pmu/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pmu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+      ),
+      home: const MyHomePage(title: 'UlybinAA PIbd-32'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  Color _color = Colors.black12;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      _color = Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: _color,//Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            if (_counter > 10 && _counter < 19)
+              Text(
+                'Stop clicking disabled!',
+                style: Theme.of(context).textTheme.headlineLarge,
+              )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        backgroundColor: _color,
+        tooltip: 'Increment',
+        child: const Icon(Icons.accessible_forward_outlined),
+      ),
+    );
+  }
+}
